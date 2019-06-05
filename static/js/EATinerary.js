dec6f54e5dab2d08dff01c9d6486617dc8a0d557
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-06-04 21:11:53 -0400

    Completed d3 selects and some button click logic

---
+++ static/js/EATinerary.js
@@ -0,0 +1,19 @@
+// Select the form
+const cityInput = d3.select("#city");
+const starsInput = d3.select("#stars");
+
+// Select generate a map
+const generateMap = d3.select("#btn btn-default btn-lg btn-block");
+
+generateMap.on("click", () => {
+    var stars = starsInput.property("value");
+    console.log(stars)
+    if(stars <= 0 && stars > 5){
+        console.log("Please enter a value greater than 0 and less than or equal to 5 :(")
+        window.alert("Please enter a value greater than 0 and less than or equal to 5 :(");
+    }
+    else {
+        
+    };
+    var city = cityInput.property("value").trim().toLowerCase();
+});
