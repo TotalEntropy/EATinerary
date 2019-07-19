5bbd8dda74430caf52c3b2c0eda2077155c77135
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-07-19 19:45:45 -0400

    Add d3 select statements

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+const outdoorSeating=d3.select('#Outdoor seating');
+const wheelchairFriendly=d3.select('#Wheelchair friendly');
+const kidFriendly=d3.select('#Kid friendly');
+const dogFriendly=d3.select('#Dog friendly');
+const music=d3.select('#Music');
+const parking=d3.select('#Parking');
+const bikeParking=d3.select('#Bike parking');
+
