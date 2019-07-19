fca7a0e38be5b2c96f111601b6bbe24ff32d73d0
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-07-19 19:08:06 -0400

    Added contact and about route

--- app.py
+++ app.py
@@ -137,5 +137,14 @@ def categories():
 
     return jsonify(results)
 
+@app.route("/about")
+def about():
+   return render_template("about.html")
+
+# Contact route
+@app.route("/contact")
+def contact():
+   return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
